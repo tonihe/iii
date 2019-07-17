e13b6022a58fc6595df2a0f043d306597be728b2
tonihe <tonihe7@gmail.com>
2019-07-16 21:56:00 -0700

    hovereffectadded

---
+++ app.js
@@ -0,0 +1,18 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity: 0.3,
+    image1: 'https://66.media.tumblr.com/0e8d36fa1c62867a4ec1290bf3da023e/tumblr_pswb132g9V1rho9f6o1_1280.jpg',
+    image2: 'https://66.media.tumblr.com/17948d9fd6463dea12b7bbadd5a2c5a9/tumblr_pjemu3B0Ha1qlbj70o1_1280.jpg',
+    displacementImage: 'https://raw.githubusercontent.com/robin-dela/hover-effect/master/images/fluid.jpg'
+
+
+    /*
+    try these imgs: 
+
+    https://66.media.tumblr.com/1e81118e167bc04cb1822424d4310dd6/tumblr_pjemsp0IKc1qlbj70o1_1280.png
+
+    https://66.media.tumblr.com/17948d9fd6463dea12b7bbadd5a2c5a9/tumblr_pjemu3B0Ha1qlbj70o1_1280.jpg
+
+    */
+});
+
